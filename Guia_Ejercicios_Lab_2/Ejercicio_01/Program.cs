67b345c4836bf8bc785f85cf510480f004041026
Paula <A308839@santandertecnologia.com.ar>
2020-03-25 21:54:26 -0300

    add ejercicios 1 2, helloworld & library

---
+++ Guia_Ejercicios_Lab_2/Ejercicio_01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    class Ejercicio_01
+    {
+        static void Main(string[] args)
+        {
+            /* Ingresar 5 números por consola, guardándolos en una variable escalar.
+             * Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio. */
+            Console.Title = "Ejercicio Nro 01";
+
+            int numero;
+            int max = 0;
+            int min = 0;
+            int acumulador = 0;
+            int i;
+            bool esNumero = false;
+
+            for (i = 0; i < 5; i++)
+            {
+                bool primeraVezPreguntada = true;
+
+                do
+                {
+                    if (!primeraVezPreguntada && !esNumero)
+                        Console.WriteLine("Error! Eso no es un numero.");
+
+                    Console.Write("Ingrese un numero: ");
+                    esNumero = int.TryParse(Console.ReadLine(), out numero);
+                    if (esNumero)
+                    {
+                        if (i == 0)
+                        {
+                            max = numero;
+                            min = numero;
+                        }
+                        if (numero > max)
+                        {
+                            max = numero;
+                        }
+                        else if (numero < min)
+                        {
+                            min = numero;
+                        }
+                        acumulador += numero;
+                        primeraVezPreguntada = true;
+                    }
+                    else primeraVezPreguntada = false;
+
+                } while (!esNumero);
+            }
+            float promedio = acumulador / (float)i;
+            Console.WriteLine("Maximo: {0} \nMinimo: {1} \nPromedio: {2:#,###.00}", max, min, promedio);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Guia_Ejercicios_Lab_2/Ejercicio_02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_02
+{
+    class Ejercicio_02
+    {
+        static void Main(string[] args)
+        {
+            /* Ingresar un número y mostrar: el cuadrado y el cubo del mismo.
+             * Se debe validar que el número sea mayor que cero, 
+             * caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".*/
+            Console.Title = "Ejercicio Nro 02";
+            int numero;
+            bool menorQueCero = false;
+            bool esNumero = true;
+            const string mensajeNoEsNumero = "Eso no es un numero...";
+            const string mensajeEsMenorQueCero = "El numero tiene que ser mayor a cero.";
+
+            do
+            {
+                if (menorQueCero || !esNumero)
+                {
+                    var mensaje = menorQueCero ? mensajeEsMenorQueCero : mensajeNoEsNumero;
+                    Console.WriteLine($"ERROR. {mensaje} ¡Reingresar numero!");
+                }
+                Console.Write("Ingrese un numero: ");
+                    
+                esNumero = int.TryParse(Console.ReadLine(), out numero);
+
+                if(esNumero) menorQueCero = numero <= 0;
+
+            } while (menorQueCero || !esNumero);
+
+            double cuadrado = Math.Pow(numero, 2);
+            double cubo = Math.Pow(numero, 3);
+            Console.WriteLine("Numero: {0:#,###.00} \nCuadrado: {1:#,###.00} \nCubo: {2:#,###.00}", numero, cuadrado, cubo);
+            Console.ReadKey();
+        }
+    }
+}
