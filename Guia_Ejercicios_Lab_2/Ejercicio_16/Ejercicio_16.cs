d918615365e6cdc3c839e91aac168d328e6e53ea
Paula <A308839@santandertecnologia.com.ar>
2020-04-03 17:37:17 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Guia_Ejercicios_Lab_2/Ejercicio_16/Ejercicio_16.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Ejercicio_16
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumnoUno = new Alumno();
+            Alumno alumnoDos = new Alumno();
+            Alumno alumnoTres = new Alumno();
+
+            alumnoUno.nombre = "Paula";
+            alumnoUno.apellido = "Ruhl";
+            alumnoUno.legajo = 111;
+
+            alumnoDos.nombre = "Luke";
+            alumnoDos.apellido = "Skywalker";
+            alumnoDos.legajo = 112;
+
+            alumnoTres.nombre = "Leia";
+            alumnoTres.apellido = "Organa";
+            alumnoTres.legajo = 113;
+
+            alumnoUno.Estudiar(7, 4);
+            alumnoDos.Estudiar(4, 10);
+            alumnoTres.Estudiar(3, 2);
+
+
+            alumnoUno.CalcularFinal();
+            alumnoDos.CalcularFinal();
+            alumnoTres.CalcularFinal();
+            Console.WriteLine(alumnoUno.Mostrar() + alumnoDos.Mostrar() + alumnoTres.Mostrar());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Guia_Ejercicios_Lab_2/Entidades/Alumno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    public class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal = -1;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        private Random generadorNotaFinal = new Random(Environment.TickCount);
+
+        public void CalcularFinal()
+        {
+            if(this.nota1 > 3 && this.nota2 > 3)
+            {
+                notaFinal = generadorNotaFinal.Next(1,10);
+            } 
+        }
+
+        public void Estudiar(byte nota1, byte nota2)
+        {
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }
+
+        public string Mostrar()
+        {
+            string mensaje = $"Alumno: {this.apellido}, {this.nombre} Legajo n°: {this.legajo}\nNota 1: {this.nota1}\nNota 2: {this.nota2}.\n";
+            mensaje = notaFinal == -1 ? $"{mensaje}Alumno desaprobado.\n" : $"{mensaje}Nota Final: {this.notaFinal}\n";
+            return mensaje;
+        }
+    }
+}
