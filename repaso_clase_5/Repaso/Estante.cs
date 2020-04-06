9e5c1457767a502f85e9b7b4f0a8dc03fc1106fe
Paula <Palu@DESKTOP-K0JUAIN>
2020-04-06 18:21:51 -0300

    add repaso

---
+++ repaso_clase_5/Repaso/Estante.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(Producto item in e.GetProductos())
+            {
+                sb.AppendLine(Producto.MostrarProducto(item));
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            foreach (Producto item in e.GetProductos())
+            {
+                if (item is null) continue;
+                if (item == p) return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            if(e != p)
+            {
+                for (int i = 0; i < e.GetProductos().Length; i++)
+                {
+                    if (e.GetProductos()[i] is null)
+                    {
+                        e.GetProductos()[i] = p;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if (e == p)
+            {
+                for (int i = 0; i < e.GetProductos().Length; i++)
+                {
+                    if (e.GetProductos()[i] == p)
+                    {
+                        e.GetProductos()[i] = null;
+                    }
+                }
+            }
+            return e;
+        }
+
+    }
+}
---
+++ repaso_clase_5/Repaso/Producto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.codigoDeBarra = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public double GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            return $"Marca: {p.GetMarca()}, Precio: {p.GetPrecio()} Codigo de barra: {(string)p}";
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            return p.GetMarca() == marca;
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            return p1.GetMarca() == p2.GetMarca() && (string)p1 == (string)p2;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+    }
+
+}
